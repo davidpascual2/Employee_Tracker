281bdaaf61b25ddab5dd09e461657947f444dcd8
dave <davidpascual200@gmail.com>
2022-07-01 13:43:54 -0700

    removed id

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE management_db;
 USE management_db;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE roles(
 );
 
 CREATE TABLE employees(
-    id INT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
-INSERT INTO 'department' (id, name)
+INSERT INTO 'department' (name)
 VALUES  ('Engineering'),
         ('Finance'),
         ('Legal'),
         ('Sales');
 
-INSERT INTO 'roles' (id, title, salary, department_id)
-VALUES  (001, 'Sales Lead', 60000, 004),
-        (002, "Salesperson", 50000, 004),
-        (003, 'Lead Engineer', 100000, 001);
-        (004, 'Software Engineer', 80000, 001),
-        (005, 'Account Manager',90000, 002),
-        (006, 'Accountant', 70000, 002),
-        (007, 'Legal Team Lead', 90000, 004),
-        (008, 'Lawyer', 70000, 004);
+INSERT INTO 'roles' (title, salary, department_id)
+VALUES  ('Sales Lead', 60000, 004),
+        ("Salesperson", 50000, 004),
+        ('Lead Engineer', 100000, 001);
+        ('Software Engineer', 80000, 001),
+        ('Account Manager',90000, 002),
+        ('Accountant', 70000, 002),
+        ('Legal Team Lead', 90000, 004),
+        ('Lawyer', 70000, 004);
 
-INSERT INTO 'employees' (id, first_name, last_name, role_id, manager_id)
-VALUES  (001, 'John', 'Doe', 001, 'null'),
-        (002, 'Aubrey', 'Graham', 002, 001),
-        (003, 'Dwayne', 'Carter', 003, 'null'),
-        (004, 'Jermaine', 'Cole', 004, 003),
-        (005, 'Jonathan', 'Kirk', 005, 'null'),
-        (006, 'Jacques', 'Webster', 006, 005),
-        (007, 'Lebron', 'James', 007, 'null'),
-        (008, 'Kawhi', 'Leonard', 008, 007),
+INSERT INTO 'employees' (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Doe', 001, 'null'),
+        ('Aubrey', 'Graham', 002, 001),
+        ('Dwayne', 'Carter', 003, 'null'),
+        ('Jermaine', 'Cole', 004, 003),
+        ('Jonathan', 'Kirk', 005, 'null'),
+        ('Jacques', 'Webster', 006, 005),
+        ('Lebron', 'James', 007, 'null'),
+        ('Kawhi', 'Leonard', 008, 007);
         
 
     
